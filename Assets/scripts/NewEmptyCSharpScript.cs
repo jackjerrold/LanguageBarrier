f859c0e8ce96e5da053bd41d290360446aa0614d
Jack Jerrold <141284583+jackjerrold@users.noreply.github.com>
2026-04-30 17:00:10 +0100

    Update NewEmptyCSharpScript.cs

--- Assets/scripts/NewEmptyCSharpScript.cs
+++ Assets/scripts/NewEmptyCSharpScript.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class Format : MonoBehaviour
 {
-    
+    public Vector2[] position;
+    public Vector2[] size;
+    public int formatNumber;
+
+    void Awake(){
+        if(size.Length != position.Length){
+            Debug.LogError($"Format {formatNumber} has diffrent sized, size and position arrays");
+        }
+   }
 }
