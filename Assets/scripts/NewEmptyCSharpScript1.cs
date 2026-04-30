74b4f2b305a2b7cbdef5c112839934d8b01bd559
Jack Jerrold <141284583+jackjerrold@users.noreply.github.com>
2026-04-30 17:01:05 +0100

    Update NewEmptyCSharpScript1.cs

--- Assets/scripts/NewEmptyCSharpScript1.cs
+++ Assets/scripts/NewEmptyCSharpScript1.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class Characters : MonoBehaviour
 {
-    
+    public Vector2[] position;
+    public Vector2[] size;
+    public Sprite[] images;
+    public string Translation;
+    public string Assigned;
 }
